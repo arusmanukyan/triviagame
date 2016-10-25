029b550ae7c1b7abb892e6780c11d1d026e79b4f
arusmanukyan <manukyan.arus@gmail.com>
2016-10-24 20:59:34 -0700

    hw5

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,29 +3,48 @@ $(document).ready(function(){
 // set timer
     var secondsLeft = 26;
     var interval = setInterval(function() {
-         document.getElementById("clockdiv").innerHTML = --secondsLeft;
+         document.getElementById("clockdiv").innerHTML = "Time remaining!  " + --secondsLeft;
 
-        if (secondsLeft <= 0)
-        {
-          document.getElementById("clockdiv").innerHTML = "Times up";
+        if (secondsLeft <= 0){
+          document.getElementById("clockdiv").innerHTML = "Time's up";
         }
 
-    this.reset =  function(sec)
-    {
+    this.reset =  function(sec){
         sec = (typeof(sec) !== "clockdiv") ? sec : 0;
         time = sec;
         generateTime(time);
     }
     }, 1000);
 
-   var correctAnswer = 0;
-   var wrongAnswer = 0;
-
-   function userAnswer() {
-    document.getElementById("correct");
-  }
- 
-    console.log("correctAnswer");
 
 
- });
+      $("input[name=answer]").change(function(){
+      if($("input[name=answer]:checked").val() === "correct1") {
+        console.log("You have selected the correct answer!");
+      } else if ($("input[name=answer]:checked").val() === "wrong"){
+        console.log("You have selected the wrong answer!");
+      }
+    });
+
+      $("input[name=answer]").change(function(){
+      if($("input[name=answer]:checked").val() === "correct2") {
+        console.log("You have selected the correct answer!");
+      } else if ($("input[name=answer]:checked").val() === "wrong"){
+        console.log("You have selected the wrong answer!");
+      }
+    });
+      $("input[name=answer]").change(function(){
+      if($("input[name=answer]:checked").val() === "correct3") {
+        console.log("You have selected the correct answer!");
+      } else if ($("input[name=answer]:checked").val() === "wrong"){
+        console.log("You have selected the wrong answer!");
+      }
+    });
+      $("input[name=answer]").change(function(){
+      if($("input[name=answer]:checked").val() === "correct4") {
+        console.log("You have selected the correct answer!");
+      } else if($("input[name=answer]:checked").val() === "wrong"){
+        console.log("You have selected the wrong answer!");
+      }
+    });
+});
