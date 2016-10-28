21ce747cccb9f9f2cff69ea8d810047963471627
arusmanukyan <manukyan.arus@gmail.com>
2016-10-27 17:57:47 -0700

    hw5

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function(){ 
 // set timer
+
     var secondsLeft = 26;
     var interval = setInterval(function() {
          document.getElementById("clockdiv").innerHTML = "Time remaining!  " + --secondsLeft;
@@ -18,33 +19,71 @@ $(document).ready(function(){
 
 
 
+    // var questions =
+    // {
+    //     question1: "What color is Mars", 
+    //     answer1:
+    //     {
+    //       yellow: "Yellow",
+    //       red: "Red",
+    //       green: "Green",
+    //     }
+    //     question2: "How old is the sun?", 
+    //     answer2:
+    //     {
+    //       two: "2 million years old",
+    //       three: "3 billion years old",
+    //       fourPointFive: "4.5 billion years old";
+    //     }
+    //     question3: "What are meteor showers?", 
+    //     answer3:
+    //     {
+    //       rocks: "Rocks",
+    //       dust: "Dust",
+    //       water: "Water";
+    //     }
+    // }
+
+
+      // console.log correct or wrong answer. 
+
       $("input[name=answer]").change(function(){
-      if($("input[name=answer]:checked").val() === "correct1") {
-        console.log("You have selected the correct answer!");
-      } else if ($("input[name=answer]:checked").val() === "wrong"){
+       if ($("input[name=answer]:checked").val() === "wrong"){
         console.log("You have selected the wrong answer!");
       }
     });
 
-      $("input[name=answer]").change(function(){
-      if($("input[name=answer]:checked").val() === "correct2") {
+      $("input[name=correct1]").change(function(){
+      if($("input[name=correct1]:checked").val() === "correct1") {
         console.log("You have selected the correct answer!");
-      } else if ($("input[name=answer]:checked").val() === "wrong"){
+      }else if ($("input[name=answer]:checked").val() === "wrong"){
         console.log("You have selected the wrong answer!");
       }
     });
-      $("input[name=answer]").change(function(){
-      if($("input[name=answer]:checked").val() === "correct3") {
+
+      $("input[name=correct2]").change(function(){
+      if($("input[name=correct2]:checked").val() === "correct2") {
         console.log("You have selected the correct answer!");
-      } else if ($("input[name=answer]:checked").val() === "wrong"){
+      }else if ($("input[name=answer]:checked").val() === "wrong"){
         console.log("You have selected the wrong answer!");
       }
     });
-      $("input[name=answer]").change(function(){
-      if($("input[name=answer]:checked").val() === "correct4") {
+      $("input[name=correct3]").change(function(){
+      if($("input[name=correct3]:checked").val() === "correct3") {
         console.log("You have selected the correct answer!");
-      } else if($("input[name=answer]:checked").val() === "wrong"){
+      }else  $("input[name=answer]").change(function(){
+       if ($("input[name=answer]:checked").val() === "wrong"){
         console.log("You have selected the wrong answer!");
       }
+    });
+    });
+      $("input[name=correct4]").change(function(){
+      if($("input[name=correct4]:checked").val() === "correct4") {
+        console.log("You have selected the correct answer!");
+      }else  $("input[name=answer]").change(function(){
+       if ($("input[name=answer]:checked").val() === "wrong"){
+        console.log("You have selected the wrong answer!");
+      }
+    });
     });
 });
